a5a038c548f89341fd80a40e17323e821ae55d15
Manodya Rathnagoda <dnsbandara03@gmail.com>
2026-01-30 10:15:29 +0530

    Update api.js

--- Lufyco_Frontend/app/api/api.js
+++ Lufyco_Frontend/app/api/api.js
@@ -3,7 +3,7 @@ import { Platform } from 'react-native';
 
 // Use localhost for web, and different IP for Android emulator/Real device if needed
 // For Android Emulator usually 10.0.2.2, for Genymotion 10.0.3.2
-const API_URL = Platform.OS === 'android' ? 'http://10.0.2.2:5000/api' : 'http://localhost:5000/api';
+const API_URL = Platform.OS === 'android' ? 'http://10.0.2.2:5001/api' : 'http://localhost:5001/api';
 
 const api = axios.create({
     baseURL: API_URL,
